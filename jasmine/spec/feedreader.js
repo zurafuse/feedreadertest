7f03093e10628eff0de0cd52a0efd73057df139c
Timothy Horton <zurafuse@gmail.com>
2017-03-27 12:43:28 -0500

    Add files via upload

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -29,10 +29,7 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         * This for loop checks the url property of each object in the allFeeds array and checks to
+        /* This for loop checks the url property of each object in the allFeeds array and checks to
 		 * make sure no .url property is defined or empty.
 		*/		 
 		it('has a defined URL that is not empty', function() {
@@ -41,10 +38,7 @@ $(function() {
 				expect(allFeeds[i].url).not.toBe("");
 			}
 		});
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         * This for loop checks the name property of each object in the allFeeds array and checks to
+        /* This for loop checks the name property of each object in the allFeeds array and checks to
 		 * make sure no .name property is defined or empty.
          */
 		it('has a defined URL that is not empty', function() {
@@ -55,24 +49,14 @@ $(function() {
 		});		 
     });
 
-
-    /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-		 * If the .menu-hidden class is available, the style sheet has the transform
+        /* If the .menu-hidden class is available, the style sheet has the transform
 		 * attribute of the slide-menu set to -12em, thus hiding it.
          */
 		it('is hidden by default', function(){
 			expect($('.menu-hidden')[0]).toBeDefined();
 		});
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-		  * Perform the same check as 'is hidden by default' but this time check
+         /* Perform the same check as 'is hidden by default' but this time check
 		  * once after a user clicks the hamburger icon to make sure .menu-hidden 
 		  * is no longer available, and check again after the user clicks a second 
 		  * time to make sure .menu-hidden has returned.
@@ -86,14 +70,9 @@ $(function() {
 			expect($('.menu-hidden')[0]).toBeDefined();
 		});
 	});
-    /* TODO: Write a new test suite named "Initial Entries" */
+
 	describe('Initial Entries', function(){
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-		 * Before each run of loadFeed(), "done' is passed as the call back.
+        /* Before each run of loadFeed(), "done' is passed as the call back.
 		 * This is to prevent expect() to run again before the previous instance
 		 * of loadFeed has completed.
          */
@@ -104,15 +83,12 @@ $(function() {
 		});
 		//make sure there is at least one entry after loadFeed is called.
 		it('exist in the .feed container when the loadFee() function is called', function(){
-			expect($('.entry-link')[0]).toBeDefined();
+			expect($('.feed .entry').length).toBeGreaterThan(0);
 		});
 	});
-    /* TODO: Write a new test suite named "New Feed Selection" */
+
 	describe('New Feed Selection', function(){
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-		 * First, before a new entry is created, assign the last entry to
+        /* First, before a new entry is created, assign the last entry to
 		 * a variable called previousEntry. Then after the function has run,
 		 * compare the last entry to what the last entry was before the function
 		 * and make sure they are not the same thing.
